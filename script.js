c1d49b8abae828eeac3a82fd3b050921c29b6854
Andrew Yu <igotyuu421@gmail.com>
2021-04-03 11:30:06 -0700

    storage fixed

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// Day planner 
-
-//sett up HTML
-    //get bootstrap cdn links
-    //layout the HTML
-        // add a jumbotron that display current day and date
-            //(e.g Wed. Mar 31st)
-    //Create Rows for hours 9-5
-        //parent div/ hour div/ text input/ save button
-    
-            //save icon
-                //bootstrap icons cdn links button
-                //font awesome link and button
-
-//Javascript
-    // set up click event on save buttons
-        //jquery .on click
-    //using event.target, traverse the dom from the button to the text area
-    //also grab a unique identifier for placing teh hour input value into local storage
-
-    // on pageload, pull all data from localStorage and use unique label identifier mentioned above to determine where you put the value
-        //should be able to use query to find children
-
-        //id to data id grabbing
-
-    // for detemining css backgroundcolor: "backgroundcolor" :unknown word.
-        //use moment to get current time (soecifically the hour)
-        //then iterate over all hour elements
-        //using an id/data attribute determining what hour the current time block represents 
-            //e.g moment time "timeblock"; unknown time
-                //if the current element time is:
-                //;ess than moment time, is grey
-                //same as moment time, its red
-                // greater than moment time its green
-
 var saveBtn = $('.saveBtn');
 var currentDay = $('#currentDay');
 var row = $('.row');
@@ -70,10 +35,31 @@ $(document).ready(function(){
     setInterval(rowColors,1000);
 });
 
-saveBtn.on('click', $(this).prev(),function(){
+saveBtn.on('click',function(){
     var rowHour= $(this).parent().data('hour');
     var saveText = $(this).prev().val();
-    localStorage.setItem(rowHour, JSON.stringify(saveText));
-    saveText= JSON.parse(localStorage.getItem(rowHour));
+    
+    //get local data
+    const data = JSON.parse(localStorage.getItem("data")) || [];
+
+    const filteredData = data.filter((datum) => datum.id !== rowHour);
+    
+    //create a new entry for
+    const entry  = {
+        id: rowHour,
+        txt: saveText
+    }
+
+    //push the entry to the
+    filteredData.push(entry);
+
+    //set the updated data and overwrite old data on storage
+    localStorage.setItem("data",JSON.stringify(filteredData));
+    console.log ("hello");
 })
+
+const data = JSON.parse(localStorage.getItem("data")) || [];
+data.forEach(datum => {
+    $(`[data-hour=${datum.id}]`).find(".text").val(datum.txt);
+});
 
